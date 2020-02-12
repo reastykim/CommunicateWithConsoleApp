dea6deea05b9a2e274bde6e844999ad5d9aac376
Reasty Kim <reastykim@orthosensor.com>
2020-02-12 14:48:24 +0900

    initial commit

--- CommunicateWithConsoleApp/MainWindow.xaml.cs
+++ CommunicateWithConsoleApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,30 @@ namespace CommunicateWithConsoleApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string ConsoleAppPath = "ConsoleApp1.exe";
+        private Process _consoleAppProcess;
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _consoleAppProcess = new Process();
+            _consoleAppProcess.EnableRaisingEvents = true;
+            _consoleAppProcess.StartInfo.FileName = ConsoleAppPath;
+            _consoleAppProcess.StartInfo.CreateNoWindow = true;
+            _consoleAppProcess.StartInfo.UseShellExecute = false;
+
+            _consoleAppProcess.Exited += (ss, ee) =>
+            {
+                Console.WriteLine();
+                Console.WriteLine($"ConsoleApp exited with ExitCode={_consoleAppProcess.ExitCode}");
+            };
+
+            var result = _consoleAppProcess.Start();
         }
     }
 }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string consoleInputString = "";
+            var random = new Random();
+
+            while (true)
+            {
+                var randomVal = random.Next(2);
+                switch (randomVal)
+                {
+                    case 0:
+                        Thread.Sleep(1000);
+                        Console.Write("나이를 입력하세요 : ");
+                        consoleInputString = Console.ReadLine();
+                        Console.WriteLine($"당신의 나이는 {consoleInputString}");
+                        break;
+                    case 1:
+                        Thread.Sleep(2500);
+                        Console.Write("이름을 입력하세요 : ");
+                        consoleInputString = Console.ReadLine();
+                        Console.WriteLine($"{consoleInputString}");
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+        }
+    }
+}
